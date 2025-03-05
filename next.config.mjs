4b578bffd58738428af78b75d3be62d86a0215f7
Lavender <alexander.hyman05@gmail.com>
2025-03-05 11:04:43 -0800

    don't validate env

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
-import { fileURLToPath } from "node:url";
-import createJiti from "jiti";
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-// Env validation
-jiti("./src/lib/env.ts")
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
