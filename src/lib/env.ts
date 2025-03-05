26efc13607e199afdd83f50d61297e2daca52ceb
Lavender <alexander.hyman05@gmail.com>
2025-03-05 11:08:39 -0800

    updates

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,12 +1,13 @@
-import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
+import dotenv from "dotenv";
 
-export const env = createEnv({
-  server: {
-    DISCORD_WEBHOOK: z.string().url(),
-    AUTHORIZATION: z.string(),
-    NODE_ENV: z.enum(["development", "production", "test"]),
-  },
-  runtimeEnv: process.env,
-  skipValidation: process.env.NODE_ENV === "development",
+dotenv.config();
+
+const envSchema = z.object({
+  DISCORD_WEBHOOK: z.string().url(),
+  AUTHORIZATION: z.string(),
+  NODE_ENV: z.enum(["development", "production", "test"]),
 });
+
+type env = z.infer<typeof envSchema>;
+export const env = envSchema.parse(process.env);
